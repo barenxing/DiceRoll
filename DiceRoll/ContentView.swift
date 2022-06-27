a7f12fac1c6364bba4f04f69439f6fb49fc235b5
Richard Hu <richard.hu@me.com>
2022-06-27 01:56:43 -0400

    initial commit

--- DiceRoll/ContentView.swift
+++ DiceRoll/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+                Text("\(Image(systemName: "die.face.1"))")
+                Text("\(Image(systemName: "die.face.2"))")
+                Text("\(Image(systemName: "die.face.3"))")
+                Text("\(Image(systemName: "die.face.4"))")
+                Text("\(Image(systemName: "die.face.5"))")
+                Text("\(Image(systemName: "die.face.6"))")
+        }
+        .font(.system(size: 100))
+        .padding()
     }
 }
 
---
+++ References.md
@@ -0,0 +1,12 @@
+#  DiceRoll
+
+This is the reference file for DiceRoll Project
+
+## Notes
+
+
+## References
+- **Swift with Majid**
+  - [SF Symbols in SwiftUI](https://swiftwithmajid.com/2021/12/21/sf-symbols-in-swiftui/)
+- **APPCODA**
+  - [如何使用 CATransform3D 處理 3D 影像、製做互動立體旋轉的效果](https://www.appcoda.com.tw/catransform3d/)
