41f7409383621bd993cc9cb5d22cba1a9290fbc4
Richard Hu <richard.hu@me.com>
2022-07-04 23:21:07 -0400

    now each die has its own rotate variable and can rotate on its own.

--- DiceRoll/ContentView.swift
+++ DiceRoll/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var dice = Array(repeating: "die.face.1", count: 3)
+    private static let diceCount = 2
+    @State private var dice = Array(repeating: "die.face.1", count: diceCount)
+    @State private var rotates = Array(repeating: false, count: diceCount)
+
     var body: some View {
         VStack {
-            ForEach(dice, id: \.self) { die in
-                Text("\(Image(systemName: die))")
+            ForEach(Array(dice.enumerated()), id: \.offset) { i, die in
+                Image(systemName: die)
+                    .resizable()
+                    .foregroundColor(.blue)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .rotationEffect(Angle.degrees(rotates[i] ? 360*5: 0))
+                    .animation(Animation.easeInOut, value: rotates[i])
+                    .padding()
+
             }
         }
-        .font(.system(size: 200))
-        .padding()
         .onTapGesture(count: 2) {
-            for i in 0...dice.count-1 {
+            for i in 0..<ContentView.diceCount {
+                rotates[i].toggle()
                 dice[i] = "die.face.\(Int.random(in: 1...6))"
             }
         }
